3a102c2a7000f4aead640adaa4a6de1ae9b42e94
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-09-11 00:10:13 +0200

    :mute: Do not display other container logs on success
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- test/docker_test.sh
+++ test/docker_test.sh
@@ -43,6 +43,6 @@ fi
 
 # Success
 echo 'Docker test successful'
-echo 'Check the following logs for details:'
-tail -n 100 logs/*.log
+#echo 'Check the following logs for details:'
+#tail -n 100 logs/*.log
 exit 0
