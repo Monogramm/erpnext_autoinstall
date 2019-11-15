558aad3fb3b8bdd59d3d02310bf0ddeb35111614
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-11-15 22:31:15 +0100

    :white_check_mark: Improve Tests execution
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- .travis/docker_test.sh
+++ .travis/docker_test.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+################################################################################
+# Testing docker containers
+
 echo "Waiting to ensure everything is fully ready for the tests..."
 sleep 60
 
@@ -34,15 +37,91 @@ if ! sudo ping -c 10 -q erpnext_web ; then
     exit 8
 fi
 
+
+################################################################################
+# Success
+echo 'Docker tests successful'
+
+
+################################################################################
+# Automated Unit tests
 # https://docs.docker.com/docker-hub/builds/automated-testing/
 # https://frappe.io/docs/user/en/testing
-# https://frappe.io/docs/user/en/guides/automated-testing/unit-testing
-echo "Executing custom app tests..."
-bench run-tests --profile --app erpnext_autoinstall
-## TODO Test result of tests
+################################################################################
 
+FRAPPE_APP_TO_TEST=erpnext_autoinstall
+
+echo "Preparing Frappe application '${FRAPPE_APP_TO_TEST}' tests..."
+
+################################################################################
+# Frappe Unit tests
+# https://frappe.io/docs/user/en/guides/automated-testing/unit-testing
+
+FRAPPE_APP_UNIT_TEST_REPORT="$(pwd)/sites/.${FRAPPE_APP_TO_TEST}_unit_tests.xml"
+FRAPPE_APP_UNIT_TEST_PROFILE="$(pwd)/sites/.${FRAPPE_APP_TO_TEST}_unit_tests.prof"
+
+#bench run-tests --help
+
+echo "Executing Unit Tests of '${FRAPPE_APP_TO_TEST}' app..."
+if [ "${TEST_VERSION}" = "10" ]; then
+    bench run-tests \
+        --app "${FRAPPE_APP_TO_TEST}" \
+        --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}" \
+        --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+else
+    bench run-tests \
+        --app "${FRAPPE_APP_TO_TEST}" \
+        --coverage \
+        --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+    # FIXME https://github.com/frappe/frappe/issues/8809
+    #    --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}"
+fi
+
+## Check result of tests
+if [ -f "${FRAPPE_APP_UNIT_TEST_REPORT}" ]; then
+    echo "Checking Frappe application '${FRAPPE_APP_TO_TEST}' unit tests report..."
+
+    if grep -E '(errors|failures)="[1-9][0-9]*"' "${FRAPPE_APP_UNIT_TEST_REPORT}"; then
+        echo "Unit Tests of '${FRAPPE_APP_TO_TEST}' app failed! See report for details:"
+        cat "${FRAPPE_APP_UNIT_TEST_REPORT}"
+        exit 1
+    else
+        echo "Unit Tests of '${FRAPPE_APP_TO_TEST}' app successful! See report for details:"
+        cat "${FRAPPE_APP_UNIT_TEST_REPORT}"
+    fi
+fi
+
+if [ -f ./sites/.coverage ]; then
+    echo "Sending Unit Tests coverage of '${FRAPPE_APP_TO_TEST}' app to Coveralls..."
+    coveralls -b "$(pwd)/apps/${FRAPPE_APP_TO_TEST}" -d ./sites/.coverage
+fi
+
+if [ -f "${FRAPPE_APP_UNIT_TEST_PROFILE}" ]; then
+    echo "Checking Frappe application '${FRAPPE_APP_TO_TEST}' unit tests profile..."
+
+    # XXX Is there any online services that could receive and display profiles?
+    cat "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+fi
+
+
+################################################################################
+# TODO QUnit (JS) Unit tests
+# https://frappe.io/docs/user/en/guides/automated-testing/qunit-testing
+
+#bench run-ui-tests --help
+
+#echo "Executing UI Tests of '${FRAPPE_APP_TO_TEST}' app..."
+#if [ "${TEST_VERSION}" = "10" ] || [ "${TEST_VERSION}" = "11" ]; then
+#    bench run-ui-tests --app ${FRAPPE_APP_TO_TEST}
+#else
+#    bench run-ui-tests ${FRAPPE_APP_TO_TEST}
+#fi
+
+## TODO Check result of UI tests
+
+
+################################################################################
 # Success
-echo 'Docker test successful'
-#echo 'Check the following logs for details:'
-#tail -n 100 logs/*.log
+echo 'Frappe app '${FRAPPE_APP_TO_TEST}' tests finished'
+echo 'Check the CI reports and logs for details.'
 exit 0
