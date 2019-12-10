094301c7576f590fdd748f47a9e7bcfd0b3a815b
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-12-10 13:23:09 +0100

    :green_heart: Do not execute UT until UT created
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- .travis/docker_test.sh
+++ .travis/docker_test.sh
@@ -62,20 +62,22 @@ FRAPPE_APP_UNIT_TEST_PROFILE="$(pwd)/sites/.${FRAPPE_APP_TO_TEST}_unit_tests.pro
 
 #bench run-tests --help
 
-echo "Executing Unit Tests of '${FRAPPE_APP_TO_TEST}' app..."
-if [ "${TEST_VERSION}" = "10" ]; then
-    bench run-tests \
-        --app "${FRAPPE_APP_TO_TEST}" \
-        --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}" \
-        --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
-else
-    bench run-tests \
-        --app "${FRAPPE_APP_TO_TEST}" \
-        --coverage \
-        --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
-    # FIXME https://github.com/frappe/frappe/issues/8809
-    #    --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}"
-fi
+# [TODO] Enable when Unit Tests added to application
+
+#echo "Executing Unit Tests of '${FRAPPE_APP_TO_TEST}' app..."
+#if [ "${TEST_VERSION}" = "10" ]; then
+#    bench run-tests \
+#        --app "${FRAPPE_APP_TO_TEST}" \
+#        --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}" \
+#        --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+#else
+#    bench run-tests \
+#        --app "${FRAPPE_APP_TO_TEST}" \
+#        --coverage \
+#        --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+#    # FIXME https://github.com/frappe/frappe/issues/8809
+#    #    --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}"
+#fi
 
 ## Check result of tests
 if [ -f "${FRAPPE_APP_UNIT_TEST_REPORT}" ]; then
@@ -100,7 +102,7 @@ if [ -f "${FRAPPE_APP_UNIT_TEST_PROFILE}" ]; then
     echo "Checking Frappe application '${FRAPPE_APP_TO_TEST}' unit tests profile..."
 
     # XXX Is there any online services that could receive and display profiles?
-    cat "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+    #cat "${FRAPPE_APP_UNIT_TEST_PROFILE}"
 fi
 
 
