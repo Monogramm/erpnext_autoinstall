045bf8f8c43b8c879a156333eded74cfab3affcf
Emil <amionv99@gmail.com>
2020-04-09 17:30:15 +0300

    :sparkles: Add list of users command
    Signed-off-by: Emil <amionv99@gmail.com>

--- erpnext_autoinstall/commands/__init__.py
+++ erpnext_autoinstall/commands/__init__.py
@@ -5,6 +5,16 @@ from click import pass_context
 from frappe.utils.user import add_role
 
 
+# def wrapper_set_user_permissions(set_user_permissions):
+#     def accept_argument(context, username=None, email=None, role=None):
+#         if email is not None and frappe.db.exists("User",
+#                                                   email) or username is not None and frappe.db.exists("User", {'username': username}):
+#         if frappe.db.exists("Role", role):
+#             set_user_permissions(context, username, email, role)
+#         else:
+#             print("Role does not exist")
+
+
 @click.command('set-user-permissions')
 @click.option('--username')
 @click.option('--email')
@@ -17,13 +27,23 @@ def set_user_permissions(context, username=None, email=None, role=None):
     """
     site = context.obj['sites'][0]
     frappe.init(site=site)
-    user = None
     frappe.connect(site)
     if username is not None:
-        add_role(username, role)
+        user = frappe.get_doc("User", {'username': username})
+        user.add_roles(role)
     if email is not None:
         add_role(email, role)
 
+@click.command('list-users')
+@pass_context
+def list_users(context):
+    site = context.obj['sites'][0]
+    frappe.init(site=site)
+    frappe.connect(site)
+    users = frappe.get_all("User")
+    for user in users:
+        print(user.name)
+
+commands = [set_user_permissions, list_users]
 
 
-commands = [set_user_permissions]
